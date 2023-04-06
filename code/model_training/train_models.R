5971b5d6d82aec58b709eee9f1ddcdbf2f00466a
pommevilla <pvillanueva13@gmail.com>
2023-04-06 16:37:28 -0500

    Add notes, save additional model objects

--- code/model_training/train_models.R
+++ code/model_training/train_models.R
@@ -22,9 +22,9 @@ source("code/model_training/model_training_helpers.R")
 hab_metrics <- metric_set(roc_auc, sens, yardstick::spec, accuracy)
 
 ######################### Preparing data
-# Reading in data
+# Reading in data. Samples without microcystin information
+# for the following week are filtered out.
 hab_data_raw <- read.csv(here("data", "data_prep", "combined_normalized.csv")) %>%
-    # Filtering out samples that don't have microcystin information for the following week
     filter(!is.na(category_d_ahead)) %>%
     mutate(category_d_ahead = as.factor(category_d_ahead))
 
@@ -32,15 +32,19 @@ hab_data_raw <- read.csv(here("data", "data_prep", "combined_normalized.csv")) %
 feature_importances <- read.csv("data/model_training/feature_importances.csv")
 
 # We say a feature is important if it's average normalized importance
-# was greater than 0. This indicates that its predictive power was greater than average.
+# was greater than 0. This indicates that its predictive power was greater than average
+# according to our resampling framework.
 important_features <- feature_importances %>%
     filter(a_n_importance > 0) %>%
     pull("variable")
 
 # Filtering down the total dataset to just the important features and the metadata needed
-# for model training
+# for model training. We rename the mcyA_16s column because that was renamed
+# after initial model training. If we were to repeat the hyperparameter tuning
+# step, we would be able to remove this step.
 hab_data <- hab_data_raw %>%
-    select(all_of(important_features), category_d_ahead)
+    select(all_of(important_features), category_d_ahead) %>%
+    rename(mcya_16s = mcyA_16s)
 
 set.seed(130)
 hab_data_split <- initial_split(hab_data, strata = category_d_ahead, prop = 0.8)
@@ -52,7 +56,9 @@ hab_test <- testing(hab_data_split)
 # We performed all of the preprocessing in the data prep steps of the pipeline
 base_recipe <- recipe(formula = category_d_ahead ~ ., data = hab_train)
 
-# Defining under- and oversampling recipes
+# Defining under- and oversampling recipes. Since we have such a small number of
+# hazardous cases, we want to try both sampling strategies to see which one
+# is better for model performance.
 downsample_recipe <- base_recipe %>%
     step_downsample(category_d_ahead)
 
@@ -60,7 +66,7 @@ oversample_recipe <- base_recipe %>%
     step_smote(category_d_ahead)
 
 ######################### Defining tidymodels models
-# Defining the XGBoost model and setting the parameters that we'll tune
+# Defining the XGBoost model and setting the parameters that we'll tune.
 xgboost_spec <- boost_tree(
     trees = tune(),
     min_n = tune(),
@@ -107,6 +113,8 @@ race_controls <- control_race(
 tuning_combinations <- 200
 workflow_fit_filename <- paste0("hab_models_1_", tuning_combinations, ".rds")
 
+# Since this process can take a while, we check for previously saved hyperparameter tuning runs
+# with the same number of tuning combinations. If it hasn't been saved, we'll run the pipeline and save it.
 if (!file.exists(here("results/model_training", workflow_fit_filename))) {
     hab_models_1 <- workflow_set(
         preproc = list(downsampled = downsample_recipe, oversampled = oversample_recipe),
@@ -123,18 +131,21 @@ if (!file.exists(here("results/model_training", workflow_fit_filename))) {
             control = race_controls
         )
 
-
     saveRDS(
         hab_models_1,
         here("results/model_training", workflow_fit_filename)
     )
 }
 
+# This loads the result of the hyperparameter tuning (based on the number of hyperparameter
+# combinations defined above)
 hab_models_1 <- readRDS(here("results/model_training", workflow_fit_filename))
 
 ######## Naive model calculations
 # Our naive model predicts the next week's status to be whatever the
-# current week's status is.
+# current week's status is. We calculate the confusion matric
+# and the ROC AUC here for later use. We'll calculate the other metrics
+# below.
 confusion_table <- table(
     hab_data_raw[, "category_d"],
     hab_data_raw[, "category_d_ahead"]
@@ -150,7 +161,10 @@ naive_auc <- auc(naive_guesses$category_d, naive_guesses$prob) %>%
 
 ######### Training Results
 # These are the results from model training with an additional row
-# for the metrics from the naive model
+# for the metrics from the naive model. Note that even though we are mostly
+# interested in the performance of the model when predicting the negative class,
+# we use ROC AUC because it is a better overall indicator of model performance, especially
+# when there are real-world economic costs to closing a beach due to a predicted HAB.
 all_results <- rank_results(hab_models_1, rank_metric = "roc_auc") %>%
     select(rank, wflow_id, .metric, mean, std_err, model) %>%
     mutate(sampling_strategy = str_split(wflow_id, "_", simplify = TRUE)[, 1]) %>%
@@ -165,7 +179,7 @@ write.csv(
 )
 
 # Training metrics again, but only selecting the best performing model from each
-# model and preprocessing type
+# model and preprocessing type.
 best_results <- rank_results(hab_models_1, select_best = TRUE, rank_metric = "roc_auc") %>%
     select(rank, wflow_id, .metric, mean, std_err, model) %>%
     mutate(sampling_strategy = str_split(wflow_id, "_", simplify = TRUE)[, 1]) %>%
@@ -186,13 +200,16 @@ write.csv(
 )
 
 ######### Creating an ensemble model
-# We'll pick the model that performs best for each of the metrics to use in our ensemble model
+# We'll explore various strategies for combing models to improve predictions.
+# We'll begin by picking the model that performs best for each of the metrics.
 best_models_by_metric <- best_results %>%
     filter(model != "naive") %>%
     group_by(.metric) %>%
     slice_max(mean, n = 1) %>%
     ungroup()
 
+# We'll begin by blending predictions. We prioritize ROC AUC for the same above reasons.
+set.seed(1736)
 ensemble_model <- stacks() %>%
     add_candidates(
         hab_models_1 %>%
@@ -203,12 +220,17 @@ ensemble_model <- stacks() %>%
     ) %>%
     fit_members()
 
+saveRDS(
+    ensemble_model,
+    here("results/model_training", "ensemble_model_prio_roc_auc.rds")
+)
+
 # Generating predictions from the ensemble model, including class probabilities
 ensemble_predictions <- hab_test %>%
     bind_cols(predict(ensemble_model, hab_test, type = "prob")) %>%
     mutate(predicted = as.factor(if_else(.pred_1 > 0.5, 1, 3)))
 
-# Get metrics from ensemble predictions
+# Get metrics from ensemble predictions on the testing set.
 ensemble_confusion_matrix <- table(
     ensemble_predictions[, "category_d_ahead"],
     ensemble_predictions[, "predicted"]
@@ -220,6 +242,8 @@ get_prediction_metrics(ensemble_predictions, category_d_ahead, .pred_1, predicte
 
 ################## Testing results
 # We'll only evaluate the testing results of the best model for each configuration.
+# These steps will redefine the models with the highest-performing hyperparameters,
+# fit it again on the training data, and then generate predictions on the testing set.
 testing_predictions_df <- hab_test %>%
     select(category_d_ahead)
 testing_results_df <- data.frame()
@@ -266,6 +290,7 @@ for (x in hab_models_1$wflow_id) {
     )
 }
 
+# Adding results for the naive prediction strategy.
 testing_results <- testing_results_df %>%
     add_row(
         sampling_strategy = "naive",
@@ -292,6 +317,7 @@ testing_results %>%
     select(model, sampling_strategy, roc_auc, accuracy, sens, spec) %>%
     arrange(desc(model), desc(sampling_strategy))
 
+# We save the results in long format for easier plotting.
 write.csv(
     testing_results,
     here("data/model_training", "testing_results.csv"),
@@ -301,11 +327,10 @@ write.csv(
 
 ######## Manual ensemble
 # Here, we'll pick the models that performed the best for each of the metrics
-# and use them to vote for the class.
+# and use them to generate hard and soft predictions.
 voting_predictions <- testing_predictions_df %>%
     select(
         category_d_ahead,
-        # contains(best_models_by_metric$wflow_id %>% unique()),
         contains("downsampled_xgboost"),
         contains("oversampled_xgboost"),
         contains("oversampled_log_reg")
@@ -315,6 +340,7 @@ voting_predictions <- testing_predictions_df %>%
         mean_prediction = as.factor(if_else(mean_pred_1 > 0.5, 1, 3))
     )
 
+# This generates the metrics for the soft predictions
 get_prediction_metrics(voting_predictions, category_d_ahead, mean_pred_1, mean_prediction)
 
 mode_predictions <- voting_predictions %>%
@@ -322,4 +348,5 @@ mode_predictions <- voting_predictions %>%
     mutate(mode = getmode(c_across(contains(".pred_class")))) %>%
     ungroup()
 
+# THis generates the metrics for the hard predictions
 get_prediction_metrics(mode_predictions, category_d_ahead, mean_pred_1, mode)
